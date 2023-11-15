e2ed113bc01a1563a0538f615feff77d93aee1b1
AurelienGuern <aguern96@gmail.com>
2023-11-15 10:25:17 +0100

    MAJ portfolio

--- app/Views/footer.tpl.php
+++
@@ -1,6 +0,0 @@
-</main>
-
-<script src="<?= $_SERVER['BASE_URI'] ?>/assets/js/script.js"></script>
-</body>
-
-</html>
--- app/Views/home.tpl.php
+++
@@ -1,8 +0,0 @@
-<div class="home">
-   
-    <div class="menu">
-        <a class="button_slide slide_right" href="<?=$router->generate('portfolio');?>">Mes créations</a>
-        
-        <a class="button_slide slide_right" href="<?=$router->generate('contact');?>">Me contacter</a>
-    </div>
-</div>
